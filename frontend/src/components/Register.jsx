42ff36f905f02aebfbf555cac2ebc15741b76884
Maximilian <scientificresearch309@gmail.com>
2022-12-06 23:53:02 +0100

    Login && Register pages looking good

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,9 +1,99 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h1>This is Register Page</h1>
+    <div className="register">
+      <div className="card">
+        <div className="card-header">
+          <h3>Register</h3>
+        </div>
+
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="username">User Name</label>
+              <input
+                type="text"
+                // onChange={inputHandle}
+                name="userName"
+                // value={state.userName}
+                className="form-control"
+                placeholder="User Name"
+                id="username"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                // onChange={inputHandle}
+                name="email"
+                // value={state.email}
+                className="form-control"
+                placeholder="Email"
+                id="email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                // onChange={inputHandle}
+                name="password"
+                // value={state.password}
+                className="form-control"
+                placeholder="Password"
+                id="password"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                // onChange={inputHandle}
+                name="confirmPassword"
+                // value={state.confirmPassword}
+                className="form-control"
+                placeholder="Confirm Password"
+                id="confirmPassword"
+              />
+            </div>
+
+            <div className="form-group">
+              <div className="file-image">
+                <div className="image">
+                  {/* {loadImage ? <img src={loadImage} /> : ""} */}
+                </div>
+                <div className="file">
+                  <label htmlFor="image">Select Image</label>
+                  <input
+                    type="file"
+                    // onChange={fileHandle}
+                    name="image"
+                    // value={state.image}
+                    className="form-control"
+                    id="image"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <input type="submit" value="register" className="btn" />
+            </div>
+
+            <div className="form-group">
+              <span>
+                <Link to="/messenger/login"> Login Your Account </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
-// import './index.css'
+// import "./index.css";
+import "./main.scss";
+import store from "./store/index.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// import { positions, transitions, Provider as AlertProvider } from "react-alert";
+// import alertTemplate from "react-alert-template-basic";
+
+// const options = {
+//   timeout: 5000,
+//   positions: positions.BOTTOM_CENTER,
+//   transitions: transitions.SCALE,
+// };
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+      <App />
+  </Provider>
+);
